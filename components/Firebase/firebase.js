96fe6166803d3262006f8be46db59638187cada2
Jae min Song <jaeminsong@Jaeui-MacBookPro.local>
2021-02-02 00:30:18 +0900

    사진주소받기/업로드/카메라/권한허용 기능추가

--- components/Firebase/firebase.js
+++ components/Firebase/firebase.js
@@ -25,12 +25,13 @@ export const auth = firebase.auth();
 
 export const db = firebase.firestore()
 
+export const storage = firebase.storage()
+
 export const loginWithEmail = (email, password) =>
   auth.signInWithEmailAndPassword(email, password);
 
 export const registerWithEmail = (email, password) =>
   auth.createUserWithEmailAndPassword(email, password);
-
 export const logout = () => auth.signOut();
 
 export const passwordReset = email => auth.sendPasswordResetEmail(email);
--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -6,17 +6,19 @@ import {
   Platform,
   TouchableOpacity,
   Text,
-  StyleSheet
+  StyleSheet,
 } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import Constants from "expo-constants"
 import { Camera } from "expo-camera"
+import * as firebase from "firebase"
+import { auth,storage } from "../components/Firebase/firebase"
 const UploadScreen = () => {
   const [image, setImage] = useState(null)
   const [hasPermission, setHasPermission] = useState(null)
   const [type, setType] = useState(Camera.Constants.Type.back)
-  const [isCameraOpen,setIsCameraOpen] = useState(false)
-
+  const [isCameraOpen, setIsCameraOpen] = useState(false)
+  const uid = auth.currentUser.uid
   useEffect(() => {
     ;(async () => {
       if (Platform.OS !== "web") {
@@ -30,10 +32,38 @@ const UploadScreen = () => {
     })()
     ;(async () => {
       const { status } = await Camera.requestPermissionsAsync()
+      console.log(status)
       setHasPermission(status === "granted")
     })()
   }, [])
 
+  const permitAgain = async()=>{
+    const {status} = await Camera.requestPermissionsAsync()
+    setHasPermission(status === "granted")
+  }
+
+  const uploadImage = async (url, name) => {
+    const res = await fetch(url)
+    const blob = await res.blob()
+
+    let ref = storage.ref().child(`images/${uid}`)
+    return ref.put(blob)
+  }
+
+  const getAndSave =  ()=>{
+
+     storage.ref().child(`images/${uid}`).getDownloadURL().then((res)=>{
+       console.log(res)
+     })
+  }
+  const openCamera = async () => {
+    let result = await ImagePicker.launchCameraAsync()
+
+    if (!result.cancelled) {
+      setImage(result.uri)
+    }
+    return result
+  }
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -42,29 +72,45 @@ const UploadScreen = () => {
       quality: 1,
     })
 
-    console.log(result)
+    console.log(result.url)
 
     if (!result.cancelled) {
       setImage(result.uri)
     }
   }
   if (hasPermission === null) {
-    return <View />
+    return <View ><Text>권한이 없써용</Text></View>
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>
+    return (<View><Text>카메라 권한이 없습니다</Text>
+    
+  <Button title="권한허용 재시도" onPress={permitAgain}/></View>)
   }
 
   return (
-      <>
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title='Pick an image from camera roll' onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
+    <>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title='사진선택' onPress={pickImage} />
 
-    </View>
-    <View style={styles.container}>
+        <Button title='url' onPress={getAndSave} />
+        <Button title='카메라열기' onPress={openCamera} />
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+        <Button
+          title='확인'
+          onPress={() => {
+            uploadImage(image, "test")
+              .then((res) => {
+                console.log(JSON.stringify(res) )
+              })
+              .catch((err) => {
+                console.log(err)
+              })
+          }}
+        />
+      </View>
+      {/* <View style={styles.container}>
         <Button title="오픈" onPress={()=>{setIsCameraOpen(true)}} />
     {isCameraOpen && <Camera style={styles.camera} type={type}>
     <View style={styles.buttonContainer}>
@@ -81,33 +127,32 @@ const UploadScreen = () => {
         </TouchableOpacity>
     </View>
     </Camera>}
-    </View>
+    </View> */}
     </>
   )
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    camera: {
-      flex: 1,
-    },
-    buttonContainer: {
-      flex: 1,
-      backgroundColor: 'transparent',
-      flexDirection: 'row',
-      margin: 20,
-    },
-    button: {
-      flex: 0.1,
-      alignSelf: 'flex-end',
-      alignItems: 'center',
-    },
-    text: {
-      fontSize: 18,
-      color: 'white',
-    },
-  });
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+})
 
 export default UploadScreen
-
